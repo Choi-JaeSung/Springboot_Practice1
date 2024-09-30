2a4d450d6a99c4fc1b4aea023e6ac8ce8e120337
SKY <skyler4784@gmail.com>
2024-09-30 21:32:08 +0900

    first commit initialize Springboot_Practice1 github

---
+++ src/test/java/practice/sky/aroundhub/AroundHubSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.sky.aroundhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundHubSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
